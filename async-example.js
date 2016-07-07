e9699df299af50506c6b93b165e2e8411b9d90c7
pajamaw <pjpeterww@gmail.com>
2016-07-07 13:32:50 -0400

    Done.

--- async-example.js
+++ async-example.js
@@ -5,3 +5,29 @@ const async   = require('async');
 const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
 const WEATHER_API_KEY = '88bfde3533d98e8a300a138ef668cda2';
 
+async.waterfall([
+  (callback) => {
+    prompt.get({
+      name: 'city',
+      description: 'Enter city to fetch its current weather'
+      //console.log("Here we'll ask for the user's input.");
+  }, (err, result) => {
+    if (err) return callback(err);
+    callback(null, result.city);
+    });
+  },
+  (city, callback) => {
+    //console.log(`the user entered ${city}`);
+    const url = WEATHER_API_URL + city + '&APPID=' + WEATHER_API_KEY +
+            '&units=imperial';
+    request(url, (err, resp, body) => {
+      if (err) return callback(err);
+      callback(null, city, body);
+    });
+    //console.log("Here we'll request the weather data.");
+  }
+], (err, city, weather) => {
+  if (err) console.error(err);
+  console.log( `The weather in ${city}: \n, ${weather}`)
+  //console.log("Here we'll output the result.");
+});
